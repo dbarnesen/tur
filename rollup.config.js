e509862565a70b998542e04fe7a14c099de02c09
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-03-27 12:37:25 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
 import { terser } from 'rollup-plugin-terser';
-
 export default {
   input: 'main.js', // Your entry point
   output: {
     file: 'dist/bundle.js', // Where to output the bundle
     format: 'iife' // Format: Immediately-Invoked Function Expression (good for web)
+    globals: {
+      'mapbox-gl': 'mapboxgl'
+    },
   },
   plugins: [terser()] // Minify the output (optional)
+  external: ['mapbox-gl'],
 };
