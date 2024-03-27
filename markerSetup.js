460e75f0da83f5c9f56987d0523a7005a19aef05
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-03-27 17:32:45 +0100

    Update markerSetup.js

--- markerSetup.js
+++ markerSetup.js
@@ -1,5 +1,6 @@
 import mapboxgl from 'mapbox-gl';
 import { createCustomMarkerElement, toggleMarkerIcon, scrollToSelectedItem, applySelectionStyling } from './markerUtils.js';
+import { selectedMarkerIcon, unselectedMarkerIcon } from './config.js';
 
 export function setupMarkers(map) {
     const collectionItems = document.querySelectorAll('.tur-collection-item');
@@ -9,14 +10,32 @@ export function setupMarkers(map) {
     collectionItems.forEach((item, index) => {
         const latitude = parseFloat(item.getAttribute('data-lat'));
         const longitude = parseFloat(item.getAttribute('data-lng'));
-        if (!isNaN(latitude) && !isNaN(longitude)) {
-            const marker = new mapboxgl.Marker({ element: createCustomMarkerElement(), anchor: 'bottom' })
-                .setLngLat([longitude, latitude])
-                .addTo(map);
+        const kategori = item.getAttribute('data-kategori'); // Example attribute
+
+        if (!isNaN(latitude) && !isNaN(longitude) && kategori) {
+            const markerElement = createCustomMarkerElement(unselectedMarkerIcon);
+            const marker = new mapboxgl.Marker({
+                element: markerElement,
+                anchor: 'bottom'
+            }).setLngLat([longitude, latitude]).addTo(map);
 
             markers.push(marker);
 
-            item.addEventListener('click', () => {
-                map.flyTo({ center: [longitude, latitude], zoom: 16 });
+            item.addEventListener('click', function() {
+                map.flyTo({
+                    center: [longitude, latitude],
+                    zoom: 16
+                });
                 scrollToSelectedItem(item);
-                selectedCollectionItem = applySelection
+                selectedCollectionItem = applySelectionStyling(item, selectedCollectionItem);
+                toggleMarkerIcon(markers, index, selectedMarkerIcon, unselectedMarkerIcon);
+            });
+
+            marker.getElement().addEventListener('click', function() {
+                item.click(); // Triggers the click event on the corresponding collection item
+            });
+        }
+    });
+
+    return markers; // Return markers for potential further use
+}
