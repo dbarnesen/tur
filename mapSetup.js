fb024838a1f9d68b9393ca742d0504e7c350df4b
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-03-27 17:22:56 +0100

    Update mapSetup.js

--- mapSetup.js
+++ mapSetup.js
@@ -2,38 +2,26 @@ import mapboxgl from 'mapbox-gl';
 import { mapboxAccessToken, mapStyle, defaultCenter, defaultZoom } from './config.js';
 
 export function initializeMap() {
-  mapboxgl.accessToken = mapboxAccessToken;
+    mapboxgl.accessToken = mapboxAccessToken;
+    const map = new mapboxgl.Map({
+        container: 'turmap',
+        style: mapStyle,
+        center: defaultCenter,
+        zoom: defaultZoom,
+        pitch: 54,
+    });
 
-  const map = new mapboxgl.Map({
-    container: 'turmap',
-    style: mapStyle,
-    center: defaultCenter,
-    zoom: defaultZoom,
-    pitch: 54,
-  });
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+    map.addControl(new mapboxgl.ScaleControl({ maxWidth: 80, unit: 'metric' }));
+    const geolocate = new mapboxgl.GeolocateControl({
+        positionOptions: { enableHighAccuracy: true },
+        trackUserLocation: true,
+        showUserLocation: true,
+        showUserHeading: true,
+    });
 
-  map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-  map.addControl(new mapboxgl.ScaleControl({
-    maxWidth: 80,
-    unit: 'metric',
-  }));
+    map.on('load', () => map.addControl(geolocate));
+    document.getElementById('geolocateButton').addEventListener('click', () => geolocate.trigger());
 
-  const geolocate = new mapboxgl.GeolocateControl({
-    positionOptions: {
-      enableHighAccuracy: true,
-    },
-    trackUserLocation: true,
-    showUserLocation: true,
-    showUserHeading: true,
-  });
-
-  map.on('load', () => {
-    map.addControl(geolocate);
-  });
-
-  document.getElementById('geolocateButton').addEventListener('click', function() {
-    geolocate.trigger();
-  });
-
-  return map;
+    return map;
 }
