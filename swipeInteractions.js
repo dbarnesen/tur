84d37163b63d5b9aed8ad79e8666666042a3b7b7
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-03-27 21:31:28 +0100

    Update swipeInteractions.js

--- swipeInteractions.js
+++ swipeInteractions.js
@@ -1,5 +1,7 @@
-// A helper function to select elements. Mimics a bit of the jQuery selector for convenience.
-const $ = selector => document.querySelector(selector);
+// A helper function to select all elements and apply a callback function. Mimics jQuery's approach for convenience.
+const $$ = (selector, callback) => {
+    document.querySelectorAll(selector).forEach(callback);
+};
 
 export function setupSwipeInteractions() {
     let startY, endY;
@@ -12,30 +14,44 @@ export function setupSwipeInteractions() {
     const handleTouchEnd = event => {
         if (!startY) return; // Prevent handling if touch start event hasn't occurred
         endY = event.changedTouches[0].clientY;
-        
+
         const deltaY = endY - startY;
         if (Math.abs(deltaY) > minSwipeDistance) {
             if (deltaY > 0) event.preventDefault();
-            deltaY < 0 ? expandDiv() : collapseDiv();
+            deltaY < 0 ? expandDiv(event.target) : collapseDiv(event.target);
             window.scrollTo(0, 0);
             startY = null;
         }
     };
 
-    const expandDiv = () => {
-        $('.tur-collection-content').style.height = '70vh';
-        $('.tur-tray-arrow').style.transform = 'rotateX(0deg)';
+    const expandDiv = (target) => {
+        // Find the closest .tur-collection-content to the event target and adjust its style
+        const content = target.closest('.tur-collection-content');
+        if (content) {
+            content.style.height = '70vh';
+        }
+        // Adjust the tray arrow within the same content block
+        const trayArrow = content.querySelector('.tur-tray-arrow');
+        if (trayArrow) {
+            trayArrow.style.transform = 'rotateX(0deg)';
+        }
     };
 
-    const collapseDiv = () => {
-        $('.tur-collection-content').style.height = '20vh';
-        $('.tur-tray-arrow').style.transform = 'rotateX(180deg)';
+    const collapseDiv = (target) => {
+        // Similar logic to expandDiv, but for collapsing
+        const content = target.closest('.tur-collection-content');
+        if (content) {
+            content.style.height = '20vh';
+        }
+        const trayArrow = content.querySelector('.tur-tray-arrow');
+        if (trayArrow) {
+            trayArrow.style.transform = 'rotateX(180deg)';
+        }
     };
 
-    // Assuming '.tur-content-slide-cnt' is the swipeable area
-    const contentSlideCnt = $('.tur-content-slide-cnt');
-    if (contentSlideCnt) {
+    // Apply the touch event listeners to all .tur-content-slide-cnt elements
+    $$('.tur-content-slide-cnt', contentSlideCnt => {
         contentSlideCnt.addEventListener('touchstart', handleTouchStart);
         contentSlideCnt.addEventListener('touchend', handleTouchEnd);
-    }
+    });
 }
