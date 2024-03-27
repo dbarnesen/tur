7b59037d73a1759f946622c8f69a72165d3737a0
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-03-27 17:38:55 +0100

    Update swipeInteractions.js

--- swipeInteractions.js
+++ swipeInteractions.js
@@ -5,53 +5,34 @@ export function setupSwipeInteractions() {
     let startY, endY;
     const minSwipeDistance = 20; // Minimum swipe distance threshold
 
-    const handleTouchStart = (event) => {
+    const handleTouchStart = event => {
         startY = event.touches[0].clientY;
     };
 
-    const handleTouchEnd = (event) => {
+    const handleTouchEnd = event => {
         if (!startY) return; // Prevent handling if touch start event hasn't occurred
         endY = event.changedTouches[0].clientY;
-
+        
         const deltaY = endY - startY;
-
-        // Check if swipe distance is greater than minimum threshold
         if (Math.abs(deltaY) > minSwipeDistance) {
-            // Prevent default behavior if downward swipe
-            if (deltaY > 0) {
-                event.preventDefault();
-            }
-
-            // If swiped up, expand the div
-            if (deltaY < 0) {
-                expandDiv();
-            } 
-            // If swiped down, collapse the div
-            else {
-                collapseDiv();
-            }
-
-            // Force the viewport to stay at the top of the page
+            if (deltaY > 0) event.preventDefault();
+            deltaY < 0 ? expandDiv() : collapseDiv();
             window.scrollTo(0, 0);
+            startY = null;
         }
-
-        // Reset startY
-        startY = null;
     };
 
-    // Function to expand the div
     const expandDiv = () => {
-        $('.tur-collection-content').style.height = '70vh'; // Adjust height as needed
-        $('.tur-tray-arrow').style.transform = 'rotateX(0deg)'; // Rotate arrow when expanded
+        $('.tur-collection-content').style.height = '70vh';
+        $('.tur-tray-arrow').style.transform = 'rotateX(0deg)';
     };
 
-    // Function to collapse the div
     const collapseDiv = () => {
-        $('.tur-collection-content').style.height = '20vh'; // Adjust height as needed
-        $('.tur-tray-arrow').style.transform = 'rotateX(180deg)'; // Reset arrow rotation
+        $('.tur-collection-content').style.height = '20vh';
+        $('.tur-tray-arrow').style.transform = 'rotateX(180deg)';
     };
 
-    // Add touch start and touch end event listeners to the expandable div
+    // Assuming '.tur-content-slide-cnt' is the swipeable area
     const contentSlideCnt = $('.tur-content-slide-cnt');
     if (contentSlideCnt) {
         contentSlideCnt.addEventListener('touchstart', handleTouchStart);
