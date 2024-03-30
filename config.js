5bbcec813d9d3566e3ef7aa916c55d70def32d64
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-03-30 14:33:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const mapboxAccessToken = 'pk.eyJ1IjoiZGJhcm5lc2VuIiwiYSI6IjFWeUJFNFUifQ.CF2Du3MPcaCQhBBNJSQMDQ';
-export const unselectedMarkerIcon = 'https://uploads-ssl.webflow.com/5a12777c9048ed000106553e/65f40a4f33d817767e6e242e_marker_blue.svg';
-export const selectedMarkerIcon = 'https://uploads-ssl.webflow.com/5a12777c9048ed000106553e/65f40a4f0116312d1320e108_marker_okre.svg';
+export const unselectedMarkerIcon = 'location_on';
+export const selectedMarkerIcon = 'location_on';
 export const mapStyle = 'mapbox://styles/dbarnesen/ckk3vfvbi4t1v17p82fuy61pt';
 export const defaultCenter = [8.2961, 59.91639];
 export const defaultZoom = 5.5;
