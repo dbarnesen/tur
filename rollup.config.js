851a189ba148fc50d0f25c4fdf6e2eb1f4e06923
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-04-07 10:01:29 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve'; // Helps Rollup find external modules
+import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
 
 export default {
   input: 'main.js', // Your entry point
@@ -6,9 +8,14 @@ export default {
     file: 'dist/bundle.js', // Where to output the bundle
     format: 'iife', // Format: Immediately-Invoked Function Expression (good for web)
     globals: {
-      'mapbox-gl': 'mapboxgl' // Correct placement of globals
+      'mapbox-gl': 'mapboxgl', // Correct placement of globals
+      'gsap': 'GSAP', // Define GSAP as a global (if you're using it from a CDN)
     },
   },
-  plugins: [terser()], // Minify the output (optional)
-  external: ['mapbox-gl'] // Tells Rollup that mapbox-gl is an external dependency
+  plugins: [
+    resolve(), // Add this before other plugins that transform your modules
+    commonjs(), // Convert CommonJS modules to ES6, so they can be included in the Rollup bundle
+    terser() // Minify the output (optional)
+  ],
+  external: ['mapbox-gl'], // Tells Rollup that mapbox-gl is an external dependency
 };
