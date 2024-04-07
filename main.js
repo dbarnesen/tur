7d2d7967c569830b7441e0984ffb39495959359c
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-04-07 09:16:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import { mapboxAccessToken } from './config.js';
 import { initializeMap } from './mapSetup.js';
 import { setupMarkers } from './markerSetup.js';
-import { setupSwipeInteractions } from './swipeInteractions.js';
+import { initSwipeInteractions } from './swipeInteractions.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     mapboxgl.accessToken = mapboxAccessToken; // Ensures mapboxgl is available globally
     const map = initializeMap();
     const markers = setupMarkers(map); // Pass map to setup markers
-    setupSwipeInteractions();
+    initSwipeInteractions();
 });
