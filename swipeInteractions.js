e899ef96728530b19caa095b542cd458f12c16ca
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-04-07 11:33:18 +0200

    Update swipeInteractions.js

--- swipeInteractions.js
+++ swipeInteractions.js
@@ -21,4 +21,31 @@ export function initSwipeInteractions() {
       }
     });
   }
-}
+};
+document.querySelectorAll('.tur-collection-item').forEach(item => {
+  item.addEventListener('click', function() {
+    const itemId = this.getAttribute('data-item-id');
+    const contentToReveal = document.querySelector(`.tur-collection-content[data-item-id="${itemId}"]`);
+    
+    // Reset and hide the previously active content
+    if (currentActiveContent && currentActiveContent !== contentToReveal) {
+      currentActiveContent.style.display = 'none';
+      gsap.to(currentActiveContent, { top: '80vh', duration: 0.5 });
+    }
+
+    // Show the newly selected content
+    if (contentToReveal) {
+      contentToReveal.style.display = 'block'; // Make the content block to reveal it
+      gsap.to(contentToReveal, { top: '50vh', duration: 0.5, onComplete: () => {
+        // Reinitialize draggable if necessary
+        Draggable.create(contentToReveal, {
+          type: "y",
+          bounds: { minY: -window.innerHeight * 0.2, maxY: 0 },
+          // Add your onDragEnd logic here
+        });
+      }});
+      
+      currentActiveContent = contentToReveal; // Update the currently active content
+    }
+  });
+});
