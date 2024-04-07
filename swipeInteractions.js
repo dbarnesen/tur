b66a2722114012d36e86c6d970ab4158f49b5095
dbarnesen <164188156+dbarnesen@users.noreply.github.com>
2024-04-07 11:00:35 +0200

    Update swipeInteractions.js

--- swipeInteractions.js
+++ swipeInteractions.js
@@ -4,19 +4,21 @@ import { Draggable } from "gsap/Draggable";
 gsap.registerPlugin(Draggable);
 
 export function initSwipeInteractions() {
-  
-  document.querySelectorAll('.tur-collection-content').forEach(bottomSheet => {
-  Draggable.create(bottomSheet, {
-    type: "y",
-    bounds: window,
-    onDragEnd: function() {
-      let snapTo = 0; // Default snap back to the starting position
-      if (Math.abs(this.y) > snapThreshold) {
-        snapTo = this.y < 0 ? -snapToPosition : snapToPosition;
-      }
-      gsap.to(bottomSheet, { y: snapTo, duration: 0.5 });
-    }
-  });
-});
+  const bottomSheet = document.querySelector('.tur-collection-content');
 
-};
+  if (bottomSheet) {
+    const viewportHeight = window.innerHeight;
+    const minY = viewportHeight * 0.2; // 20vh from the top
+    const maxY = viewportHeight * 0.7; // Initial position at 70vh from the top
+
+    Draggable.create(bottomSheet, {
+      type: "y",
+      bounds: { minY: -minY, maxY: -maxY },
+      onDragEnd: function() {
+        if (this.y < -minY) {
+          gsap.to(bottomSheet, { y: -minY, duration: 0.5 }); // Snap back if dragged too far
+        }
+      }
+    });
+  }
+}
